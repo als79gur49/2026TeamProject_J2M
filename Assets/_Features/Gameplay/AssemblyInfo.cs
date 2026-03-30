ea1df2325d7f38d30b5f4803e2c06b42bf2cb150
als79gur49 <alsgur7949@naver.com>
2026-03-30 22:01:41 +0900

    chore: Project - PlayMode 테스트 friend assembly 추가

--- Assets/_Features/Gameplay/AssemblyInfo.cs
+++ Assets/_Features/Gameplay/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Game.Feature.Gameplay.Host")]
 [assembly: InternalsVisibleTo("Game.Feature.Gameplay.Tests")]
+[assembly: InternalsVisibleTo("Game.Feature.Gameplay.PlayModeTests")]
