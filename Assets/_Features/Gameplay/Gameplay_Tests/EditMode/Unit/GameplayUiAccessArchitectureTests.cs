7b02bf770463eadacacb27320f6c172bf020e40f
als79gur49 <alsgur7949@naver.com>
2026-04-15 00:32:23 +0900

    test: UI - gameplay access boundary 검증 추가
    - public surface에서 forbidden gameplay internals와 vocabulary leakage를 감시하는 architecture test를 추가했다.
    - HUD read path, pause admission, topology feed, player slice feed를 확인하는 runtime test를 추가했다.
    - UIAccess assembly를 직접 검증할 수 있도록 필요한 EditMode test asmdef 참조만 보강했다.

---
+++ Assets/_Features/Gameplay/Gameplay_Tests/EditMode/Unit/GameplayUiAccessArchitectureTests.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Game.Feature.Gameplay.BoardState;
+using Game.Feature.Gameplay.Entities;
+using Game.Feature.Gameplay.Loop;
+using Game.Feature.Gameplay.PlayerControl;
+using Game.Feature.Gameplay.UIAccess.Contracts;
+using NUnit.Framework;
+
+namespace Game.Feature.Gameplay.Tests.Unit
+{
+    public sealed class GameplayUiAccessArchitectureTests
+    {
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_PublicSurface_DoesNotExposeForbiddenGameplayInternals()
+        {
+            var forbiddenTypes = new HashSet<Type>
+            {
+                typeof(WorldState),
+                typeof(WorldSnapshot),
+                typeof(TickResult),
+                typeof(TickInputBuffer),
+                typeof(TickInput),
+                typeof(PlayerTickCommand),
+                typeof(TickPresentationData),
+                typeof(PlayerControlState),
+                typeof(EntityExecutionLockState),
+                typeof(EnemyActionRuntimeState),
+                typeof(EnemyJumpRuntimeState),
+                typeof(PlayerDamageState),
+                typeof(IWorldWriteContext),
+            };
+            var forbiddenTypeNames = new HashSet<string>
+            {
+                "Game.Feature.Gameplay.Loop.MovementPhaseResult",
+                "Game.Feature.Gameplay.Loop.AttackPhaseResult",
+                "Game.Feature.Gameplay.Debug.TickTrace",
+            };
+            var assembly = typeof(IGameplayCommandGateway).Assembly;
+            var leakedTypes = assembly
+                .GetExportedTypes()
+                .SelectMany(GetPublicSurfaceTypes)
+                .Select(NormalizeType)
+                .Where(type => type != null &&
+                               (forbiddenTypes.Contains(type) ||
+                               forbiddenTypeNames.Contains(type.FullName) ||
+                               (type.Namespace != null && type.Namespace.StartsWith("Game.Feature.Gameplay.Host", StringComparison.Ordinal))))
+                .Distinct()
+                .ToArray();
+
+            Assert.That(leakedTypes, Is.Empty);
+        }
+
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_PublicNames_DoNotLeakForbiddenVocabulary()
+        {
+            var forbiddenTokens = new[]
+            {
+                "Snapshot",
+                "RuntimeState",
+                "Occupancy",
+                "PhaseResult",
+                "Commit",
+                "InputBuffer",
+                "Host",
+            };
+            var assembly = typeof(IGameplayCommandGateway).Assembly;
+            var leakedNames = assembly
+                .GetExportedTypes()
+                .SelectMany(type => GetPublicNames(type))
+                .Where(name => forbiddenTokens.Any(token => name.IndexOf(token, StringComparison.OrdinalIgnoreCase) >= 0))
+                .Distinct()
+                .ToArray();
+
+            Assert.That(leakedNames, Is.Empty);
+        }
+
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_QueryFacade_OnlyExposesBoundedReaderProperties()
+        {
+            var facadeType = typeof(IGameplayQueryFacade);
+            var publicInstanceMethods = facadeType
+                .GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                .Where(method => !method.IsSpecialName)
+                .ToArray();
+            var properties = facadeType
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                .OrderBy(property => property.Name)
+                .ToArray();
+
+            Assert.That(publicInstanceMethods, Is.Empty);
+            CollectionAssert.AreEqual(
+                new[] { "Objectives", "PlayerHud", "Session" },
+                properties.Select(property => property.Name).ToArray());
+            Assert.That(properties.All(property => property.PropertyType.IsInterface), Is.True);
+            Assert.That(properties.All(property => property.PropertyType.Name.EndsWith("Query", StringComparison.Ordinal)), Is.True);
+        }
+
+        private static IEnumerable<string> GetPublicNames(Type type)
+        {
+            yield return type.Name;
+
+            foreach (var property in type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                yield return property.Name;
+            }
+
+            foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                if (!method.IsSpecialName)
+                {
+                    yield return method.Name;
+                }
+            }
+        }
+
+        private static IEnumerable<Type> GetPublicSurfaceTypes(Type type)
+        {
+            yield return type;
+
+            foreach (var property in type.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                foreach (var surfacedType in ExpandType(property.PropertyType))
+                {
+                    yield return surfacedType;
+                }
+            }
+
+            foreach (var constructor in type.GetConstructors(BindingFlags.Instance | BindingFlags.Public))
+            {
+                foreach (var parameter in constructor.GetParameters())
+                {
+                    foreach (var surfacedType in ExpandType(parameter.ParameterType))
+                    {
+                        yield return surfacedType;
+                    }
+                }
+            }
+
+            foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly))
+            {
+                if (!method.IsSpecialName)
+                {
+                    foreach (var surfacedType in ExpandType(method.ReturnType))
+                    {
+                        yield return surfacedType;
+                    }
+
+                    foreach (var parameter in method.GetParameters())
+                    {
+                        foreach (var surfacedType in ExpandType(parameter.ParameterType))
+                        {
+                            yield return surfacedType;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<Type> ExpandType(Type type)
+        {
+            var normalizedType = NormalizeType(type);
+            if (normalizedType == null)
+            {
+                yield break;
+            }
+
+            yield return normalizedType;
+
+            if (normalizedType.IsGenericType)
+            {
+                foreach (var argument in normalizedType.GetGenericArguments())
+                {
+                    foreach (var surfacedType in ExpandType(argument))
+                    {
+                        yield return surfacedType;
+                    }
+                }
+            }
+        }
+
+        private static Type NormalizeType(Type type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            if (type.IsByRef)
+            {
+                type = type.GetElementType();
+            }
+
+            if (type.IsArray)
+            {
+                type = type.GetElementType();
+            }
+
+            return Nullable.GetUnderlyingType(type) ?? type;
+        }
+    }
+}
---
+++ Assets/_Features/Gameplay/Gameplay_Tests/EditMode/Unit/GameplayUiAccessRuntimeTests.cs
@@ -0,0 +1,252 @@
+using System;
+using System.Collections.Generic;
+using Game.Feature.Gameplay.BoardState;
+using Game.Feature.Gameplay.Entities;
+using Game.Feature.Gameplay.Host;
+using Game.Feature.Gameplay.Loop;
+using Game.Feature.Gameplay.Objectives;
+using Game.Feature.Gameplay.PlayerControl;
+using Game.Feature.Gameplay.UIAccess.Models;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Game.Feature.Gameplay.Tests.Unit
+{
+    public sealed class GameplayUiAccessRuntimeTests
+    {
+        [Test]
+        [Category("Extended")]
+        public void GameplaySceneHost_Initialize_ExposesUiAccessQueriesWithCommittedHudState()
+        {
+            var hostObject = new GameObject("GameplaySceneHost_Initialize_ExposesUiAccessQueriesWithCommittedHudState");
+
+            try
+            {
+                var host = hostObject.AddComponent<GameplaySceneHost>();
+                host.Initialize(CreateConfiguration(new[]
+                {
+                    CreatePlayerEntity(new SurfaceCell(FaceId.Floor, 0, 0), facing: Direction.Right),
+                }));
+
+                var session = host.UiAccess.QueryFacade.Session.Read();
+                var playerHud = host.UiAccess.QueryFacade.PlayerHud.Read();
+                var objectives = host.UiAccess.QueryFacade.Objectives.Read();
+
+                Assert.That(session.NextTickIndex, Is.EqualTo(1));
+                Assert.That(session.IsPaused, Is.False);
+                Assert.That(session.CanAcceptGameplayCommands, Is.True);
+                Assert.That(session.IsStageCleared, Is.False);
+
+                Assert.That(playerHud.IsAvailable, Is.True);
+                Assert.That(playerHud.PlayerEntityId, Is.EqualTo(10));
+                Assert.That(playerHud.CurrentHp, Is.EqualTo(3));
+                Assert.That(playerHud.Facing, Is.EqualTo(Direction.Right));
+                Assert.That(playerHud.ActiveActionKind, Is.EqualTo(PlayerActionKind.None));
+                Assert.That(playerHud.CanMoveThisTick, Is.True);
+                Assert.That(playerHud.CanStartActionThisTick, Is.True);
+
+                Assert.That(objectives.HasObjective, Is.False);
+                Assert.That(objectives.IsCleared, Is.False);
+            }
+            finally
+            {
+                UnityEngine.Object.DestroyImmediate(hostObject);
+            }
+        }
+
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_PauseRejectsActionableCommands_AllowsClear_AndClearsPendingUiInput()
+        {
+            var hostObject = new GameObject("GameplayUiAccess_PauseRejectsActionableCommands_AllowsClear_AndClearsPendingUiInput");
+
+            try
+            {
+                var host = hostObject.AddComponent<GameplaySceneHost>();
+                host.Initialize(CreateConfiguration(new[]
+                {
+                    CreatePlayerEntity(new SurfaceCell(FaceId.Floor, 0, 0), facing: Direction.Right),
+                }));
+
+                var commandGateway = host.UiAccess.CommandGateway;
+                var pauseService = host.UiAccess.PauseService;
+
+                Assert.That(commandGateway.SetHeldMoveDirection(Direction.Right).Accepted, Is.True);
+
+                pauseService.Pause();
+
+                var rejectedMove = commandGateway.SetHeldMoveDirection(Direction.Up);
+                var clearAcceptance = commandGateway.ClearHeldMoveDirection();
+                var pausedSession = host.UiAccess.QueryFacade.Session.Read();
+
+                Assert.That(rejectedMove.Accepted, Is.False);
+                Assert.That(rejectedMove.RejectionReason, Is.EqualTo(GameplayCommandRejectionReason.Paused));
+                Assert.That(clearAcceptance.Accepted, Is.True);
+                Assert.That(pausedSession.IsPaused, Is.True);
+                Assert.That(pausedSession.CanAcceptGameplayCommands, Is.False);
+                Assert.That(host.InputHost.RunSingleTick(), Is.Null);
+
+                pauseService.Resume();
+
+                var resumedTick = host.InputHost.RunSingleTick();
+                var snapshotAfter = GameplayCompositionRoot.CreateSnapshot(host.WorldState);
+
+                Assert.That(resumedTick, Is.Not.Null);
+                Assert.That(snapshotAfter.TryGetEntity(10, out var player), Is.True);
+                Assert.That(player.position, Is.EqualTo(new SurfaceCell(FaceId.Floor, 0, 0)));
+            }
+            finally
+            {
+                UnityEngine.Object.DestroyImmediate(hostObject);
+            }
+        }
+
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_PresentationFeed_RotationTickPublishesTopologySliceAndStateChanges()
+        {
+            var hostObject = new GameObject("GameplayUiAccess_PresentationFeed_RotationTickPublishesTopologySliceAndStateChanges");
+
+            try
+            {
+                var host = hostObject.AddComponent<GameplaySceneHost>();
+                host.Initialize(CreateConfiguration(new[]
+                {
+                    CreatePlayerEntity(new SurfaceCell(FaceId.Floor, 0, 1), facing: Direction.Up),
+                }));
+
+                var frames = new List<GameplayPresentationFrame>();
+                var states = new List<GameplayPresentationState>();
+                host.UiAccess.PresentationFeed.FramePublished += frames.Add;
+                host.UiAccess.PresentationFeed.StateChanged += states.Add;
+
+                var acceptance = host.UiAccess.CommandGateway.SetHeldMoveDirection(Direction.Up);
+                var tickResult = host.InputHost.RunSingleTick();
+
+                Assert.That(acceptance.Accepted, Is.True);
+                Assert.That(tickResult, Is.Not.Null);
+                Assert.That(frames.Count, Is.EqualTo(1));
+                Assert.That(frames[0].Topology.HasValue, Is.True);
+                Assert.That(frames[0].Topology.Value.RotationKind, Is.EqualTo(CubeRotationKind.Forward));
+                Assert.That(frames[0].Topology.Value.DestinationTopology, Is.EqualTo(new CubeTopologyState(FaceId.Front)));
+                Assert.That(frames[0].FinalTopology, Is.EqualTo(new CubeTopologyState(FaceId.Front)));
+                Assert.That(host.UiAccess.PresentationFeed.CurrentState.IsTopologyTransitionActive, Is.True);
+                Assert.That(states.Count, Is.GreaterThanOrEqualTo(1));
+
+                host.Presenter.UpdatePresentation(host.TimingProfile.TopologyMotionDurationSeconds);
+
+                Assert.That(host.UiAccess.PresentationFeed.CurrentState.IsTopologyTransitionActive, Is.False);
+                Assert.That(states.Count, Is.GreaterThanOrEqualTo(2));
+            }
+            finally
+            {
+                UnityEngine.Object.DestroyImmediate(hostObject);
+            }
+        }
+
+        [Test]
+        [Category("Extended")]
+        public void GameplayUiAccess_PresentationFeed_MapsPlayerSlice_ForHeldMove()
+        {
+            var hostObject = new GameObject("GameplayUiAccess_PresentationFeed_MapsPlayerSlice_ForHeldMove");
+
+            try
+            {
+                var host = hostObject.AddComponent<GameplaySceneHost>();
+                host.Initialize(CreateConfiguration(new[]
+                {
+                    CreatePlayerEntity(new SurfaceCell(FaceId.Floor, 0, 0), facing: Direction.Up),
+                },
+                boardBounds: new BoardBounds(new Vector2Int(0, 0), new Vector2Int(2, 0))));
+
+                GameplayPresentationFrame capturedFrame = default;
+                var frameCount = 0;
+                host.UiAccess.PresentationFeed.FramePublished += frame =>
+                {
+                    capturedFrame = frame;
+                    frameCount++;
+                };
+
+                var acceptance = host.UiAccess.CommandGateway.SetHeldMoveDirection(Direction.Right);
+                var tickResult = host.InputHost.RunSingleTick();
+
+                Assert.That(acceptance.Accepted, Is.True);
+                Assert.That(tickResult, Is.Not.Null);
+                Assert.That(tickResult.PresentationData.PlayerLocomotionSignals.Count, Is.EqualTo(1));
+                Assert.That(tickResult.PresentationData.PlayerLocomotionSignals[0].ShouldPlayWalkLoop, Is.True);
+                Assert.That(tickResult.PresentationData.PlayerLocomotionSignals[0].MoveMotionGeneratedThisTick, Is.True);
+                Assert.That(frameCount, Is.EqualTo(1));
+                Assert.That(capturedFrame.Player.HasValue, Is.True);
+                Assert.That(capturedFrame.Player.Value.ActiveActionKind, Is.EqualTo(PlayerActionKind.None));
+                Assert.That(capturedFrame.Player.Value.ShouldPlayWalkLoop, Is.True);
+                Assert.That(capturedFrame.Player.Value.MoveMotionGeneratedThisTick, Is.True);
+                Assert.That(capturedFrame.Player.Value.ActionDirection, Is.EqualTo(Direction.None));
+                Assert.That(capturedFrame.Player.Value.TargetEntityId, Is.EqualTo(0));
+                Assert.That(capturedFrame.Player.Value.StartedThisTick, Is.False);
+                Assert.That(capturedFrame.Player.Value.ExecutedThisTick, Is.False);
+            }
+            finally
+            {
+                UnityEngine.Object.DestroyImmediate(hostObject);
+            }
+        }
+
+        private static GameplaySceneHostConfiguration CreateConfiguration(
+            EntityState[] initialEntities,
+            StageObjectiveRuntimeDefinition objectiveDefinition = null,
+            BoardBounds? boardBounds = null)
+        {
+            return new GameplaySceneHostConfiguration
+            {
+                AutoAdvanceTicks = false,
+                AutoCreateViews = false,
+                InitialBoardBounds = boardBounds ?? new BoardBounds(new Vector2Int(0, 0), new Vector2Int(1, 1)),
+                InitialEntities = initialEntities,
+                InitialTopology = new CubeTopologyState(FaceId.Floor),
+                ObjectiveRuntimeDefinition = objectiveDefinition ?? StageObjectiveRuntimeDefinition.Disabled,
+                PlayerEntityId = 10,
+            };
+        }
+
+        private static EntityState CreatePlayerEntity(
+            SurfaceCell position,
+            Direction facing = Direction.Up)
+        {
+            return new EntityState
+            {
+                entityId = 10,
+                position = position,
+                hp = 3,
+                maxHp = 3,
+                teamId = 1,
+                type = EntityType.Unit,
+                unitRole = UnitRole.Player,
+                state = EntityPhaseState.Idle,
+                facing = facing,
+                boardPresence = EntityBoardPresence.Occupying,
+                aiMode = EnemyAiMode.None,
+            };
+        }
+
+        private static EntityState CreateBoxEntity(
+            SurfaceCell position,
+            BoxCapabilities capabilities)
+        {
+            return new EntityState
+            {
+                entityId = 20,
+                position = position,
+                hp = 1,
+                maxHp = 1,
+                teamId = 0,
+                type = EntityType.Box,
+                unitRole = UnitRole.None,
+                state = EntityPhaseState.Idle,
+                facing = Direction.Up,
+                boardPresence = EntityBoardPresence.Occupying,
+                boxCapabilities = capabilities,
+                aiMode = EnemyAiMode.None,
+            };
+        }
+    }
+}
