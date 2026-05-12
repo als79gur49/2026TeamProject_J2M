7d0e08ffc2d7492035fead2ce0abb0a20d7cbd5a
als79gur49 <alsgur7949@naver.com>
2026-05-13 03:51:28 +0900

    feat: UI - MainMenu SaveSlot 키보드 내비게이션 추가
    - MainMenuScreenView 내부에 Commands와 SaveSlots focus domain을 추가해 router target을 하나로 유지했다.
    - SaveSlotPanelView와 SaveSlotCardView가 카드 행 이동과 Primary/Delete action focus를 로컬로 처리하도록 분리했다.
    - MainMenuScreen.prefab의 SaveSlotCard에 Primary/Delete SelectionFrame을 연결해 keyboard focus가 명확히 표시되도록 했다.
    - SaveSlotPanel open 중 방향키가 command group으로 fallthrough하지 않아 Start/Settings/Quit focus 오동작을 막는다.

--- Assets/_Features/UI/UI_Screens/Runtime/SaveSlotPanelView.cs
+++ Assets/_Features/UI/UI_Screens/Runtime/SaveSlotPanelView.cs
@@ -1,4 +1,5 @@
 using System;
+using Game.Feature.UI.ViewShared;
 using UnityEngine;
 
 namespace Game.Feature.UI.Screens
@@ -13,15 +14,25 @@ namespace Game.Feature.UI.Screens
         [SerializeField] private SaveSlotCardView[] _slotCards = Array.Empty<SaveSlotCardView>();
 
         private SaveSlotPanelViewModel _viewModel;
+        private int _selectedCardIndex;
+        private SaveSlotActionSelection _selectedAction = SaveSlotActionSelection.Primary;
+        private bool _navigationFrameVisible;
 
         public event Action<SaveSlotIntent> SaveSlotIntentRequested;
 
         public SaveSlotCardView[] SlotCards => _slotCards;
 
+        public bool HasFocusableCards => FindFirstFocusableCardIndex() >= 0;
+
+        public int SelectedCardIndex => _selectedCardIndex;
+
+        public SaveSlotActionSelection SelectedAction => _selectedAction;
+
         public void Bind(SaveSlotPanelViewModel viewModel)
         {
             _viewModel = viewModel;
             Refresh();
+            RefreshNavigationAfterSlotDataChanged();
         }
 
         public void ValidateAuthoredStructureOrThrow()
@@ -57,7 +68,128 @@ namespace Game.Feature.UI.Screens
 
         private void OnDisable()
         {
+            HideNavigationFrames();
             UnwireSlotCards();
+        }
+
+        public bool FocusFirstAvailableCardPrimary(bool showFrame)
+        {
+            var index = FindFirstFocusableCardIndex();
+            if (index < 0)
+            {
+                HideNavigationFrames();
+                return false;
+            }
+
+            SelectCard(index, SaveSlotActionSelection.Primary, showFrame);
+            return true;
+        }
+
+        public bool HandleNavigate(UiNavigationCommand command)
+        {
+            if (!HasFocusableCards)
+            {
+                return false;
+            }
+
+            switch (command)
+            {
+                case UiNavigationCommand.Up:
+                    MoveCard(-1);
+                    return true;
+
+                case UiNavigationCommand.Down:
+                    MoveCard(1);
+                    return true;
+
+                case UiNavigationCommand.Left:
+                    GetSelectedCard()?.MoveActionLeft();
+                    SyncSelectedActionFromCard();
+                    return true;
+
+                case UiNavigationCommand.Right:
+                    GetSelectedCard()?.MoveActionRight();
+                    SyncSelectedActionFromCard();
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public bool HandleSubmit()
+        {
+            var card = GetSelectedCard();
+            return card != null && card.SubmitSelectedAction();
+        }
+
+        public bool HandleCancel()
+        {
+            HideNavigationFrames();
+            return true;
+        }
+
+        public void HideNavigationFrames()
+        {
+            _navigationFrameVisible = false;
+            if (_slotCards == null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < _slotCards.Length; i++)
+            {
+                _slotCards[i]?.HideNavigationFrames();
+            }
+        }
+
+        public bool RefreshNavigationAfterSlotDataChanged()
+        {
+            if (_slotCards == null || _slotCards.Length == 0)
+            {
+                HideNavigationFrames();
+                return false;
+            }
+
+            var card = GetSelectedCard();
+            if (card != null && card.HasAnyFocusableAction)
+            {
+                SelectCard(_selectedCardIndex, _selectedAction, _navigationFrameVisible);
+                return true;
+            }
+
+            var fallbackIndex = FindNearestFocusableCardIndex(_selectedCardIndex);
+            if (fallbackIndex < 0)
+            {
+                HideNavigationFrames();
+                return false;
+            }
+
+            SelectCard(fallbackIndex, SaveSlotActionSelection.Primary, _navigationFrameVisible);
+            return true;
+        }
+
+        public bool RestorePreviousFocusOrFallback()
+        {
+            return RefreshNavigationAfterSlotDataChanged();
+        }
+
+        public bool RestorePreviousFocusOrFallback(bool showFrame)
+        {
+            var previousFrameVisible = _navigationFrameVisible;
+            _navigationFrameVisible = showFrame;
+            if (RefreshNavigationAfterSlotDataChanged())
+            {
+                return true;
+            }
+
+            _navigationFrameVisible = previousFrameVisible;
+            return false;
+        }
+
+        public void CaptureNavigationFocus()
+        {
+            SyncSelectedActionFromCard();
         }
 
         private void WireSlotCards()
@@ -115,6 +247,139 @@ namespace Game.Feature.UI.Screens
                 if (_slotCards[i] != null)
                 {
                     _slotCards[i].Bind(_viewModel.SlotCards[i]);
+                }
+            }
+        }
+
+        private void MoveCard(int delta)
+        {
+            if (delta == 0 || _slotCards == null || _slotCards.Length == 0)
+            {
+                return;
+            }
+
+            var nextIndex = FindNextFocusableCardIndex(_selectedCardIndex, delta);
+            if (nextIndex < 0)
+            {
+                SelectCard(_selectedCardIndex, _selectedAction, _navigationFrameVisible);
+                return;
+            }
+
+            SelectCard(nextIndex, _selectedAction, _navigationFrameVisible);
+        }
+
+        private void SelectCard(int index, SaveSlotActionSelection desiredAction, bool showFrame)
+        {
+            if (_slotCards == null || index < 0 || index >= _slotCards.Length || _slotCards[index] == null)
+            {
+                HideNavigationFrames();
+                return;
+            }
+
+            _selectedCardIndex = index;
+            _navigationFrameVisible = showFrame;
+
+            for (var i = 0; i < _slotCards.Length; i++)
+            {
+                if (_slotCards[i] == null)
+                {
+                    continue;
+                }
+
+                if (i == _selectedCardIndex)
+                {
+                    _slotCards[i].SetActionSelection(desiredAction, showFrame);
+                    _selectedAction = _slotCards[i].CurrentSelection;
+                }
+                else
+                {
+                    _slotCards[i].HideNavigationFrames();
+                }
+            }
+        }
+
+        private SaveSlotCardView GetSelectedCard()
+        {
+            if (_slotCards == null || _selectedCardIndex < 0 || _selectedCardIndex >= _slotCards.Length)
+            {
+                return null;
+            }
+
+            return _slotCards[_selectedCardIndex];
+        }
+
+        private void SyncSelectedActionFromCard()
+        {
+            var card = GetSelectedCard();
+            if (card != null)
+            {
+                _selectedAction = card.CurrentSelection;
+            }
+        }
+
+        private int FindFirstFocusableCardIndex()
+        {
+            if (_slotCards == null)
+            {
+                return -1;
+            }
+
+            for (var i = 0; i < _slotCards.Length; i++)
+            {
+                if (_slotCards[i] != null && _slotCards[i].HasAnyFocusableAction)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private int FindNearestFocusableCardIndex(int origin)
+        {
+            if (_slotCards == null || _slotCards.Length == 0)
+            {
+                return -1;
+            }
+
+            var clamped = Mathf.Clamp(origin, 0, _slotCards.Length - 1);
+            if (_slotCards[clamped] != null && _slotCards[clamped].HasAnyFocusableAction)
+            {
+                return clamped;
+            }
+
+            for (var distance = 1; distance < _slotCards.Length; distance++)
+            {
+                var lower = clamped - distance;
+                if (lower >= 0 && _slotCards[lower] != null && _slotCards[lower].HasAnyFocusableAction)
+                {
+                    return lower;
+                }
+
+                var upper = clamped + distance;
+                if (upper < _slotCards.Length && _slotCards[upper] != null && _slotCards[upper].HasAnyFocusableAction)
+                {
+                    return upper;
+                }
+            }
+
+            return -1;
+        }
+
+        private int FindNextFocusableCardIndex(int origin, int delta)
+        {
+            var index = Mathf.Clamp(origin, 0, _slotCards.Length - 1);
+            while (true)
+            {
+                index += delta;
+                if (index < 0 || index >= _slotCards.Length)
+                {
+                    return -1;
+                }
+
+                if (_slotCards[index] != null && _slotCards[index].HasAnyFocusableAction)
+                {
+                    return index;
                 }
             }
         }
