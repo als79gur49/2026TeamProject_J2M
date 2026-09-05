3b7e34eae70fcf38213f5f1fb7ff070e2ea88a68
als79gur49 <alsgur7949@naver.com>
2026-09-06 00:32:41 +0900

    fix: UI - 벨트 전환 중 지정된 배치 위치 보존
    - 벨트 갱신과 전환 완료 시 위치를 원점으로 덮어쓰던 문제를 해결했다.
    - Prefab의 수평 오프셋을 적용하고 초기 배치 위치를 기준으로 전환과 복귀를 계산하도록 맞췄다.
    - 정방향과 역방향 전환, 중단, 재바인딩에서 지정 위치가 유지되는 회귀 테스트를 추가했다.

--- Assets/_Features/UI/UI_Tests/EditMode/HUDControllerTests.cs
+++ Assets/_Features/UI/UI_Tests/EditMode/HUDControllerTests.cs
@@ -355,6 +355,70 @@ namespace Game.Feature.UI.Tests
             }
         }
 
+        [TestCase(SurfaceBeltDirection.Forward)]
+        [TestCase(SurfaceBeltDirection.Backward)]
+        public void SurfaceBeltIndicator_PreservesAuthoredPositionDuringRefreshAndTransitions(
+            SurfaceBeltDirection direction)
+        {
+            var rootObject = new GameObject("SurfaceBeltIndicator_AuthoredPosition");
+            try
+            {
+                CreateCanonicalRootView(rootObject, out var hudView);
+                var beltView = hudView.SurfaceBeltIndicatorView;
+                var content = beltView.BeltContent;
+                var authoredPosition = new Vector2(17.5f, 12.0f);
+                content.anchoredPosition = authoredPosition;
+                var presenter = new SurfaceBeltIndicatorPresenter();
+                beltView.Bind(presenter.ViewModel);
+                Assert.That(content.anchoredPosition, Is.EqualTo(authoredPosition));
+
+                LayoutRebuilder.ForceRebuildLayoutImmediate(content);
+                var step = Mathf.Abs(
+                    ((RectTransform)beltView.Cells[4].transform).anchoredPosition.y -
+                    ((RectTransform)beltView.Cells[3].transform).anchoredPosition.y);
+                Assert.That(step, Is.GreaterThan(0.0f));
+                var easeField = typeof(SurfaceBeltIndicatorView)
+                    .GetField("_animationEase", BindingFlags.Instance | BindingFlags.NonPublic);
+                easeField.SetValue(beltView, Enum.Parse(easeField.FieldType, "Linear"));
+                SetPrivateField(beltView, "_animationDurationSeconds", 1.0f);
+
+                presenter.Apply(new SurfaceBeltSnapshot(0, 0, 1, direction, true, 1));
+                var moveField = typeof(SurfaceBeltIndicatorView)
+                    .GetField("_moveTween", BindingFlags.Instance | BindingFlags.NonPublic);
+                var move = moveField.GetValue(beltView);
+                var tweenExtensions = moveField.FieldType.Assembly.GetType("DG.Tweening.TweenExtensions");
+                Assert.That(tweenExtensions, Is.Not.Null);
+                var gotoMethod = tweenExtensions.GetMethod("Goto", new[] { moveField.FieldType, typeof(float), typeof(bool) });
+                var completeMethod = tweenExtensions.GetMethod("Complete", new[] { moveField.FieldType, typeof(bool) });
+                Assert.That(gotoMethod, Is.Not.Null);
+                Assert.That(completeMethod, Is.Not.Null);
+                Assert.That(move, Is.Not.Null);
+                gotoMethod.Invoke(null, new[] { move, (object)0.5f, false });
+                Assert.That(content.anchoredPosition.x, Is.EqualTo(authoredPosition.x));
+                Assert.That(content.anchoredPosition.y, Is.EqualTo(
+                    authoredPosition.y + (direction == SurfaceBeltDirection.Forward ? -step : step) * 0.5f)
+                    .Within(0.001f));
+
+                completeMethod.Invoke(null, new[] { move, (object)true });
+                Assert.That(content.anchoredPosition, Is.EqualTo(authoredPosition));
+                presenter.Apply(new SurfaceBeltSnapshot(1, 1, 1, SurfaceBeltDirection.None, false, 1));
+                Assert.That(content.anchoredPosition, Is.EqualTo(authoredPosition));
+
+                presenter.Apply(new SurfaceBeltSnapshot(1, 1, 2, direction, true, 2));
+                move = moveField.GetValue(beltView);
+                gotoMethod.Invoke(null, new[] { move, (object)0.5f, false });
+                presenter.Apply(new SurfaceBeltSnapshot(2, 2, 2, SurfaceBeltDirection.None, false, 2));
+                Assert.That(content.anchoredPosition, Is.EqualTo(authoredPosition),
+                    "An interrupted transition must restore the authored position.");
+                beltView.Bind(new SurfaceBeltViewModel());
+                Assert.That(content.anchoredPosition, Is.EqualTo(authoredPosition));
+            }
+            finally
+            {
+                DestroySupportObjects(rootObject);
+            }
+        }
+
         [Test]
         public void HUDPrefab_AuthorsChanceAndSurfaceBeltIndicatorModulesInPrefabHierarchy()
         {
